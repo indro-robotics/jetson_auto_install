0a2ffbd2d65cfe0f04ae2c22b3ab4e086a592abb
Austin Greisman <92941098+austin-InDro@users.noreply.github.com>
2022-02-02 16:01:28 -0500

    Update Jetson_Auto_setup.sh
    Adding Wireguard support

--- Jetson_Auto_setup.sh
+++ Jetson_Auto_setup.sh
@@ -178,6 +178,10 @@ catkin_make
 
 # You'll still need to change and connect packages for autonomous to work properly... TODO for later
 
+
+# Setting Up WireGuard VPN
+sudo apt-get install wireguard openresolv -y
+
 # Connecting Rocos
 echo "Setting Up Rocos.."
 echo "You will need to interact with the terminal"
